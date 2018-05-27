d657fb973200c08f25d106cc847acb5db2023dec
Sami <sami1592@gmail.com>
2018-05-27 19:46:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration configuration = new Configuration();
+
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+
+		
+		StreamSpeech(configuration);
+//		LiveSpeech(configuration);
+		
+	}
+	
+	private static void StreamSpeech(Configuration configuration) throws IOException {
+		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+		InputStream stream = new FileInputStream(new File("bug02.wav"));
+		
+		recognizer.startRecognition(stream);
+		SpeechResult result;
+		while ((result = recognizer.getResult()) != null) {
+			System.out.format("Hypothesis: %s\n", result.getHypothesis());
+			String temp = result.getResult().getBestPronunciationResult();
+			System.out.format("Hypothesis: %s\n", temp);
+		}
+	
+		recognizer.stopRecognition();
+		
+	}
+
+	private static void LiveSpeech(Configuration configuration) throws IOException
+	{
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		// Start recognition process pruning previously cached data.
+		recognizer.startRecognition(true);
+		
+		System.out.println("Live Speech starts now");
+		
+		while(true) {
+			SpeechResult result = recognizer.getResult();
+			String utterance = result.getHypothesis();
+			
+			if(utterance.startsWith("exit")) {
+				System.out.println("Live Speech ends now");
+				break;
+			}
+			else {
+				System.out.println("Hypothesis: " + utterance);
+			}
+		}
+		
+		
+		// Pause recognition process. It can be resumed then with startRecognition(false).
+		recognizer.stopRecognition();		
+	}
+}
