4c7e5b2d3879aa5b5ae3d9baa1ebe29dda974694
Sami <sami1592@gmail.com>
2018-06-08 13:39:57 +0200

    Moves everything to YAZ, changes HelloPhoneOutput.sh to adapt to this

--- pocketsphinx-5prealpha/pocketsphinx-5prealpha/YAZ/HelloPhonemeOutput.sh
+++ pocketsphinx-5prealpha/pocketsphinx-5prealpha/YAZ/HelloPhonemeOutput.sh
@@ -1,32 +1,31 @@
 pocketsphinx_continuous -infile HelloAudio/Hello0.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello1.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello2.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello3.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello_a0.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello_a1.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello_a2.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
 
 pocketsphinx_continuous -infile HelloAudio/Hello_a3.wav -hmm  model/en-us/en-us \
                         -allphone model/en-us/en-us-phone.lm.bin  -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> HelloPhonemeOutput
-
+                        -beam 1e-20 -pbeam 1e-20 -lw 2.0 >> YAZ/HelloPhonemeOutput
